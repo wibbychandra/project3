54247f3e8516436c4a2f98016442f75413be58e8
Kevin <kevinsuwandi6@gmail.com>
2021-01-25 20:12:20 +0700

    First commit

---
+++ biodata.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Biodata {
+
+    public static void main(String[] args) {
+  
+      
+      Scanner input_bio=new Scanner (System.in);
+        System.out.println("Biodata Siswa CCIT yang ikut seminar");
+        System.out.println("----------------");
+     
+      
+        
+      
+      System.out.println("Nama = Acika Difa");
+      System.out.println("Nim = 1920013212");
+        System.out.println("Kelas = 3SC1");
+        System.out.println("Jurusan = Software Enginering ");
+        System.out.println("\n");
+        System.out.println("--- Print Biodata seminar----");
+    }
+    }
